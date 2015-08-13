01b19b4026e34a031615116f0300d5848f7e4f61
Brett Zamir <brettz9@yahoo.com>
2015-08-12 22:05:12 -0700

    allow user process method w/o monkey-patching

--- world-creator.js
+++ world-creator.js
@@ -4,9 +4,13 @@
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function WorldCreator () {
+    function WorldCreator (cfg) {
         if (!(this instanceof WorldCreator)) {
             return new WorldCreator();
+        }
+        cfg = cfg || {};
+        if (cfg.process) {
+            this.process = cfg.process;
         }
     }
     WorldCreator.prototype.processJSON = function (json) {
